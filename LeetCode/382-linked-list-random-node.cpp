dd95736f394db2c3164508e0d22373c2ed834614
Harry <68451409+lujiazho@users.noreply.github.com>
2022-01-06 17:48:33 -0800

    Add files via upload

---
+++ LeetCode/382-linked-list-random-node.cpp
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// this is called RESERVOIR SAMPLING, the math behind it is interesting
+////////////////
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+///////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    ListNode* h;
+    Solution(ListNode* head) {
+        h = head;
+    }
+    
+    int getRandom() {
+        int val = h->val;
+        int i = 2;
+        ListNode* next = h->next;
+        while(next){
+            if (rand()*1.0 / RAND_MAX < 1.0/i){ // we can calculate that the probability for each number is equal
+                val = next->val;
+            }
+            i += 1;
+            next = next->next;
+        }
+        return val;
+    }
+};
