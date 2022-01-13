b8a71ddce09888411572cff5fa57c09d7fb2cea9
Harry <68451409+lujiazho@users.noreply.github.com>
2022-01-12 19:59:46 -0800

    Add files via upload

---
+++ LeetCode/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,49 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// greedy algorithm
+////////////////
+// Time Complexity: O(nlogn), most time spent on sorting
+// Space Complexity: O(logn)
+///////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    static bool compare_2_vals(vector<int>& a, vector<int>& b){
+        if (a[1] != b[1])
+            return (a[1] < b[1]);
+        return (a[0] < b[0]);
+    }
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), compare_2_vals);
+        int res = 0, arrow = -1;
+        for (int i=0 ; i<points.size() ; i++){
+            arrow = points[i][1];
+            res += 1;
+            i += 1;
+            while (i<points.size() && points[i][0] <= arrow){
+                i += 1;
+            }
+            i -= 1;
+        }
+        return res;
+    }
+};
+
+// or start from 1, but the algorithm is basically the same; also we don't need to sort by the x_start
+class Solution {
+public:
+    static bool compare_2_vals(vector<int>& a, vector<int>& b){
+        return (a[1] < b[1]);
+    }
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), compare_2_vals);
+        int res = 1, arrow = points[0][1];
+        for (int i=1 ; i<points.size() ; i++){
+            while (i<points.size() && points[i][0] <= arrow)
+                i += 1;
+            if (i < points.size()){
+                arrow = points[i][1];
+                res += 1;
+            }
+        }
+        return res;
+    }
+};
