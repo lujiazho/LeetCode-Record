4400471f53048e0eef8d34794afe0127ce7f880a
Harry <68451409+lujiazho@users.noreply.github.com>
2022-01-20 19:18:36 -0800

    Add files via upload

---
+++ LeetCode/134-gas-station.cpp
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// half brute force, with a little optimization of recording potential start points
+////////////////
+// Time Complexity: O(n^2), this is the worse situation
+// Space Complexity: O(n)
+///////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        vector<int> diff;
+        int sum_diff = 0;
+        vector<int> pot_s; // record potential start point
+        int cur_idx = -1;
+        for (int i=0 ; i<gas.size() ; i++){
+            int di = gas[i]-cost[i];
+            diff.push_back(di);
+            sum_diff += di;
+            if (di < 0)
+                cur_idx = -1;
+            else
+                if (cur_idx == -1){
+                    cur_idx = i;
+                    pot_s.push_back(i);
+                }
+        }
+        if (sum_diff < 0) return -1; // if this < 0, we can never travel around
+        for (int i=0 ; i<pot_s.size() ; i++){ // start from pot_s[i]
+            int acc = 0, flag = 1;
+            for (int j=pot_s[i] ; j<gas.size() ; j++){
+                acc += diff[j];
+                if (acc < 0){
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag)
+                for (int j=0 ; j<pot_s[i] ; j++){
+                    acc += diff[j];
+                    if (acc < 0){
+                        flag = 0;
+                        break;
+                    }
+                }
+            if (flag) return pot_s[i];
+        }
+        return -1;
+    }
+};
