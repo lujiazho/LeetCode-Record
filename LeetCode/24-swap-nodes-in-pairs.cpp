be22c01dd136417fac572aa620a7bd91858be4fb
Harry <68451409+lujiazho@users.noreply.github.com>
2022-02-15 19:02:34 -0800

    Add files via upload

---
+++ LeetCode/24-swap-nodes-in-pairs.cpp
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// not hard
+////////////////
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+///////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode *i = head, *j = head->next;
+        i->next = j->next;
+        j->next = i;
+        head = j;
+        while (i->next && i->next->next){
+            j = i->next;
+            i->next = j->next;
+            j->next = j->next->next;
+            i->next->next = j;
+            ListNode* tmp = i;
+            i = j;
+            j = tmp;
+        }
+        return head;
+    }
+};
