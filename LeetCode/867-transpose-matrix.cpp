bd9a695a0d393afda1c96d95d75b2fb2f91b06c4
Harry <68451409+lujiazho@users.noreply.github.com>
2022-06-02 08:29:24 -0700

    Add files via upload

---
+++ LeetCode/867-transpose-matrix.cpp
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// easy
+////////////////
+// Time Complexity: O(m*n)
+// Space Complexity: O(m*n)
+///////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        vector<vector<int>> res(matrix[0].size(), vector<int>(matrix.size(), 0));
+        for (int i=0 ; i<matrix.size() ; i++){
+            for (int j=0 ; j<matrix[0].size() ; j++){
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+};
