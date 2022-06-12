03d23b0dbc2efadb362b3391c61c35c438a32e23
Harry <68451409+lujiazho@users.noreply.github.com>
2022-06-11 17:11:09 -0700

    Update 1658-minimum-operations-to-reduce-x-to-zero.cpp

--- LeetCode/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ LeetCode/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,21 +1,34 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////
-// easy
+// complex edge cases
 ////////////////
 // Time Complexity: O(n)
 // Space Complexity: O(1)
 ///////////////////////////////////////////////////////////////////////////////////////////////
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int i=0, j=numbers.size()-1;
-        int cur = numbers[i] + numbers[j];
-        while (cur != target){
-            if (cur > target)
-                j -= 1;
-            else
-                i += 1;
-            cur = numbers[i] + numbers[j];
+    int minOperations(vector<int>& nums, int x) {
+        int sum_middle = accumulate(nums.begin(), nums.end(), 0) - x;
+        if (sum_middle < 0) return -1;
+        int i=0, j=0;
+        int cur_sum = 0;
+        int max_w = -1;
+        while (j < nums.size()){
+            cur_sum += nums[j];
+            if (cur_sum < sum_middle){
+                j ++;
+            }else if (cur_sum == sum_middle){
+                max_w = max(max_w, j-i+1);
+                j ++;
+            }else{
+                while (i <= j && cur_sum > sum_middle){
+                    cur_sum -= nums[i];
+                    i ++;
+                }
+                if (cur_sum == sum_middle)
+                    max_w = max(max_w, j-i+1);
+                j ++;
+            }
         }
-        return vector<int>{i+1, j+1};
+        return max_w==-1?-1:nums.size() - max_w;
     }
 };
