c6351770a82ba262c9c2d5f4935becdd6147e357
Harry <68451409+lujiazho@users.noreply.github.com>
2022-06-13 20:36:37 -0700

    Add files via upload

---
+++ LeetCode/583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// classic dp problem
+////////////////
+// Time Complexity: O(m*n)
+// Space Complexity: O(m*n)
+///////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        
+        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
+        for (int i=1 ; i<m+1 ; i++){
+            for (int j=1 ; j<n+1 ; j++){
+                if (word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return m+n-2*dp[m][n];
+    }
+};
