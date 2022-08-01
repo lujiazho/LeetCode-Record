db3840c83f67cfa04b7d4e45b9c17ef03084f9b5
Harry <68451409+lujiazho@users.noreply.github.com>
2022-08-01 13:42:24 -0700

    Update 215-kth-largest-element-in-an-array.cpp

--- LeetCode/215-kth-largest-element-in-an-array.cpp
+++ LeetCode/215-kth-largest-element-in-an-array.cpp
@@ -11,3 +11,33 @@ public:
         return nums[nums.size()-k];
     }
 };
+
+// use heap
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        vector<int> heap(nums.begin(), nums.end());
+        for (int i=heap.size()/2-1 ; i>=0 ; i--) down(heap, i);
+        int val;
+        while (k--){
+            val = heap[0];
+            heap[0] = heap.back();
+            heap.pop_back();
+            down(heap, 0);
+        }
+        return val;
+    }
+    void down(vector<int>& heap, int idx){
+        int t = idx, size = heap.size();
+        if (idx*2+1 < size && heap[idx*2+1] > heap[t])
+            t = idx*2+1;
+        if (idx*2+2 < size && heap[idx*2+2] > heap[t])
+            t = idx*2+2;
+        if (t != idx){
+            int tmp = heap[idx];
+            heap[idx] = heap[t];
+            heap[t] = tmp;
+            down(heap, t);
+        }
+    }
+};
