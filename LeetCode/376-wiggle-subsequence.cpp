f993e440130687a15d50a2b2b29f3ff98286d7f5
Harry <68451409+lujiazho@users.noreply.github.com>
2022-08-02 12:37:58 -0700

    Update 376-wiggle-subsequence.cpp

--- LeetCode/376-wiggle-subsequence.cpp
+++ LeetCode/376-wiggle-subsequence.cpp
@@ -81,3 +81,16 @@ public:
         return flag==1?res:res + 1;
     }
 };
+
+// this is crazy
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int size=nums.size(), peak=1, valley=1;
+        for(int i=1; i<size; ++i){
+            if(nums[i]>nums[i-1]) peak = valley + 1;
+            else if(nums[i]<nums[i-1]) valley = peak + 1;
+        }
+        return max(peak, valley);
+    }
+};
