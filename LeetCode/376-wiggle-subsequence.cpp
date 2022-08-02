f446c22501872aac673096f2932551f117af3213
Harry <68451409+lujiazho@users.noreply.github.com>
2022-08-02 12:33:02 -0700

    Update 376-wiggle-subsequence.cpp

--- LeetCode/376-wiggle-subsequence.cpp
+++ LeetCode/376-wiggle-subsequence.cpp
@@ -36,3 +36,48 @@ public:
         return res;
     }
 };
+
+// a little more clear
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        if (nums.size() < 3) {
+            if (nums.size() == 1) return 1;
+            return nums[0]==nums[1]?1:nums.size();
+        }
+        
+        // starting point
+        int res = 1;
+        
+        // state: 1 increase, -1 decrease
+        int state = 0;
+        
+        // for a case that all vals are the same
+        int flag = 0;
+        if (nums[1] - nums[0]>0) state = 1;
+        if (nums[1] - nums[0]<0) state = -1;
+        if (state == 0) flag = 1;
+
+        // start from 2
+        for (int i=2 ; i<nums.size() ; i++){
+            if (state == 1) { // increase
+                if (nums[i] - nums[i-1] < 0) { // 0 doesn't change state
+                    state = -1;
+                    res += 1;
+                }
+            }else if (state == -1) { // decrease
+                if (nums[i] - nums[i-1] > 0) { // 0 doesn't change state
+                    state = 1;
+                    res += 1;
+                }
+            }else{ // the edge case that starting from flat ground
+                if (nums[i] - nums[i-1] < 0 || nums[i] - nums[i-1] > 0) {
+                    // change state depending on slope
+                    state = nums[i] - nums[i-1] < 0?-1:1;
+                    res += 1;
+                }
+            }
+        }
+        return flag==1?res:res + 1;
+    }
+};
