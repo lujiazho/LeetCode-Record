5db6d5ddc5eac0498b704266bc331effbf768a5b
Harry <68451409+lujiazho@users.noreply.github.com>
2022-08-03 11:09:05 -0700

    Update 876-middle-of-the-linked-list.py

--- LeetCode/876-middle-of-the-linked-list.py
+++ LeetCode/876-middle-of-the-linked-list.py
@@ -17,3 +17,12 @@ class Solution:
                 m_num += 1
                 m_node = m_node.next
         return m_node
+    
+# i don't know how i figure above solution out, but it could be improved by fast and slow pointer
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast, slow = head, head
+        while (fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+        return slow
