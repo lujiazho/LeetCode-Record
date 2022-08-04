e364444dfd8174b0113bdc023a8682d8975c4bbf
Harry <68451409+lujiazho@users.noreply.github.com>
2022-08-04 11:56:16 -0700

    Update 1658-minimum-operations-to-reduce-x-to-zero.cpp

--- LeetCode/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ LeetCode/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -32,3 +32,24 @@ public:
         return max_w==-1?-1:nums.size() - max_w;
     }
 };
+
+// update - 8/4/2022 - more concise version
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int sum_middle = accumulate(nums.begin(), nums.end(), 0) - x;
+        if (sum_middle < 0) return -1;
+        int i=0, j=0;
+        int cur_sum = 0;
+        int res = -1;
+        while (j < nums.size()){
+            cur_sum += nums[j];
+            while (i <= j && cur_sum > sum_middle)
+                cur_sum -= nums[i++];
+            if (cur_sum == sum_middle)
+                res = max(res, j-i+1);
+            j ++;
+        }
+        return res==-1?-1:nums.size() - res;
+    }
+};
