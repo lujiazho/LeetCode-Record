a8c047c9d8b1d58ccb9abc4b473d62566dda954d
Harry <68451409+lujiazho@users.noreply.github.com>
2022-10-08 08:34:36 -0700

    Update 16-3sum-closest.cpp

--- LeetCode/16-3sum-closest.cpp
+++ LeetCode/16-3sum-closest.cpp
@@ -1,18 +1,28 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////
-// just presum, but the range is large, so we use ordered map data structure in c++
+// use sort to reduce one loop
 ////////////////
-// Time Complexity: O(n)
-// Space Complexity: O(n)
+// Time Complexity: O(n^2)
+// Space Complexity: O(logn)
 ///////////////////////////////////////////////////////////////////////////////////////////////
-class MyCalendarThree {
-    map<int, int> timeline;
+class Solution {
 public:
-    int book(int s, int e) {
-        timeline[s]++; // 1 new event will be starting at [s]
-        timeline[e]--; // 1 new event will be ending at [e];
-        int ongoing = 0, k = 0;
-        for (pair<int, int> t : timeline)
-            k = max(k, ongoing += t.second);
-        return k;
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        
+        int n = nums.size(), ans = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < n-2; ++i) {
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                int sum3 = nums[i] + nums[l] + nums[r];
+                if (abs(ans - target) > abs(sum3 - target)) ans = sum3;
+                
+                // already got best
+                if (sum3 == target) break;
+                // move to be close to target
+                if (sum3 > target) r--;
+                else l++;
+            }
+        }
+        return ans;
     }
 };
