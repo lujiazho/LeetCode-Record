08fe96fe7c7c3c9878be2f995ed84678c9596fbc
Harry <68451409+lujiazho@users.noreply.github.com>
2022-10-10 08:44:34 -0700

    Add files via upload

---
+++ LeetCode/1328-break-a-palindrome.cpp
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// easy recursion
+////////////////
+// Time Complexity: O(n)
+// Space Complexity: O(logn)
+///////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    unordered_set<int> s;
+    bool findTarget(TreeNode* root, int k) {
+        if (!root) return false;
+        if (s.count(k - root->val)) return true;
+        s.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
+    }
+};
