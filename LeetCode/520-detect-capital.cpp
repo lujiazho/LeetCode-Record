7ad56a4d6f4c7b0928ea7826474b8e4580dc964c
Harry <68451409+lujiazho@users.noreply.github.com>
2023-01-02 08:16:36 -0800

    Update 520-detect-capital.cpp

--- LeetCode/520-detect-capital.cpp
+++ LeetCode/520-detect-capital.cpp
@@ -9,14 +9,19 @@ public:
     bool detectCapitalUse(string word) {
         int num_C = 0, first_loc = -1;
         for (int i=0 ; i<word.length() ; i++){
+            // record number of Capitals and the idx of the first one
             if ('A' <= word[i] && word[i] <= 'Z'){
                 num_C += 1;
                 if (first_loc == -1) first_loc = i;
             }
         }
+        // if no capital
         if (!num_C) return true;
+        // if all capitals
         if (num_C == word.length()) return true;
+        // if only one and it's at the first location
         if (num_C == 1 && !first_loc) return true;
+        // not valid
         return false;
     }
 };
