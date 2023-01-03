e63191fa67338a105215b96fd42f863fd3430820
Harry <68451409+lujiazho@users.noreply.github.com>
2023-01-03 08:32:02 -0800

    Add files via upload

---
+++ LeetCode/944-delete-columns-to-make-sorted.cpp
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// easy
+////////////////
+// Time Complexity: O(n*length), length is of each string
+// Space Complexity: O(1)
+///////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int delete_count = 0;
+        int row = strs.size();
+        int col = strs[0].size();
+        
+        for (int j=0 ; j<col ; j++) {
+            for (int i=0 ; i<row-1 ; i++) {
+                if(strs[i][j] > strs[i+1][j]) {
+                    delete_count++;
+                    break;
+                }
+            }
+        }
+        return delete_count;
+    }
+};
