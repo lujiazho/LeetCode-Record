86eddb92a9a38aeccd78a64fef0408d8b6f3a2c6
Harry <68451409+lujiazho@users.noreply.github.com>
2023-01-06 08:39:48 -0800

    Add files via upload

---
+++ LeetCode/1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// easy
+////////////////
+// Time Complexity: O(nlogn)
+// Space Complexity: O(logn)
+///////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int count = 0;
+        sort(costs.begin(),costs.end());
+
+        for(int i=0;i<costs.size();i++){
+            if (costs[i] > coins) break;
+            coins -= costs[i];
+            count++;
+        }
+        return count;
+    }
+};
