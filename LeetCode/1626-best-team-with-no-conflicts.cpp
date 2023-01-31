7353a062536a88e63dcb71413b38de2152269812
Harry <68451409+lujiazho@users.noreply.github.com>
2023-01-31 09:29:17 -0800

    Update 1626-best-team-with-no-conflicts.cpp

--- LeetCode/1626-best-team-with-no-conflicts.cpp
+++ LeetCode/1626-best-team-with-no-conflicts.cpp
@@ -1,17 +1,27 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////
-// easy
+// not hard
 ////////////////
-// Time Complexity: O(n)
+// Time Complexity: O(n^2)
 // Space Complexity: O(n)
 ///////////////////////////////////////////////////////////////////////////////////////////////
 class Solution {
 public:
-    int v[40];
-    int tribonacci(int n) {
-        if (n == 0) return 0;
-        if (n == 1 || n == 2) return 1;
-        if (v[n] != 0) return v[n];
-        else v[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
-        return v[n];
+    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        int n = scores.size();
+        // dp[i]: maximum scores when choosing from first i people (ends with ith one)
+        int dp[n], ans = 0;
+        vector<pair<int, int>> players;
+
+        for(int i = 0; i < n; i++) players.push_back({ages[i], scores[i]});
+        sort(players.begin(), players.end());
+
+        for(int i = 0; i < n; i++) {
+            dp[i] = players[i].second;
+            for(int j = 0; j < i; j++) {
+                if(players[j].second <= players[i].second) dp[i] = max(dp[i], dp[j] + players[i].second);
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
     }
 };
