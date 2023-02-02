d985823623cba2793cbde897af11d6d7eca54272
Harry <68451409+lujiazho@users.noreply.github.com>
2023-02-02 08:27:50 -0800

    Add files via upload

---
+++ LeetCode/953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,25 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// easy
+////////////////
+// Time Complexity: O(nlogn)
+// Space Complexity: O(logn)
+///////////////////////////////////////////////////////////////////////////////////////////////
+unordered_map<char, int> m;
+bool cmp(string a, string b){
+    int n = min(a.length(), b.length());
+    for(int i = 0; i < n; i++){
+        if(m[a[i]] < m[b[i]]) return true;
+        else if(m[a[i]] > m[b[i]]) return false;
+    }
+    if(a.length() <= b.length()) return true;
+    return false;
+}
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        for(int i = 0; i < order.length(); i++) m[order[i]] = i;
+        vector<string> temp(words.begin(), words.end());
+        sort(temp.begin(), temp.end(), cmp);
+        return temp == words;
+    }
+};
