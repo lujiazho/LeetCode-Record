0d2af4af97c24aefc4cead60705e9b35c7910a3e
Harry <68451409+lujiazho@users.noreply.github.com>
2023-02-04 08:28:22 -0800

    Update 567-permutation-in-string.cpp

--- LeetCode/567-permutation-in-string.cpp
+++ LeetCode/567-permutation-in-string.cpp
@@ -7,15 +7,19 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        // store each character in s1
         unordered_map<char, int> m;
         for (int i=0 ; i<s1.length() ; i++) m[s1[i]] += 1;
+        // sliding window in s2
         unordered_map<char, int> window;
         int num = 0;
         for (int i=0 ; i<s2.length() ; i++){
             num += 1;
             window[s2[i]] += 1;
+            // if length of sliding window is the same as s1
             if (num == s1.length()){
                 int flag = 1;
+                // check 1 to 1 mapping, utilizing hashmap instead of sorting
                 for (auto per:m){
                     if (window[per.first] != per.second){
                         flag = 0;
@@ -23,6 +27,7 @@ public:
                     }
                 }
                 if (flag) return true;
+                // removing left element in window
                 num -= 1;
                 window[s2[i - num]] -= 1;
             }
