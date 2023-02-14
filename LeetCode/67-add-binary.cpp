c14d14f776ed9bf3aeb020ebb7be9afb0a15cb4e
Harry <68451409+lujiazho@users.noreply.github.com>
2023-02-14 09:14:45 -0800

    Update 67-add-binary.cpp

--- LeetCode/67-add-binary.cpp
+++ LeetCode/67-add-binary.cpp
@@ -7,35 +7,27 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int c = 0; // carry
+        int c = 0, nex; // carry
         int idx_a = a.length()-1, idx_b = b.length()-1;
         vector<int> v;
+        // simulate addition
         while (idx_a >= 0 && idx_b >= 0){
-            int nex = c + int(a[idx_a]) + int(b[idx_b]) - 96;
+            nex = c + int(a[idx_a--]) + int(b[idx_b--]) - 96;
             v.push_back(nex%2);
             c = nex/2;
-            idx_a -= 1;
-            idx_b -= 1;
         }
+        // one still has left
         while (idx_a >= 0 || idx_b >= 0){
-            int nex = 0;
-            if (idx_a >= 0){
-                nex = c+int(a[idx_a--])-48;
-            }else if (idx_b >= 0){
-                nex = c+int(b[idx_b--])-48;
-            }else{
-                break;
-            }
+            nex = 0;
+            if (idx_a >= 0) nex = c+int(a[idx_a--])-48;
+            if (idx_b >= 0) nex = c+int(b[idx_b--])-48;
             v.push_back(nex%2);
             c = nex/2;
         }
-        if (c > 0){
-            v.push_back(1);
-        }
+        if (c > 0) v.push_back(1);
+        
         string res = "";
-        for (int i=v.size()-1 ; i>=0 ; i--){
-            res += char(v[i]+48);
-        }
+        for (int i=v.size()-1 ; i>=0 ; i--) res += char(v[i]+48);
         return res;
     }
 };
