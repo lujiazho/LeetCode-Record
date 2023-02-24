626f090821268022be179b6aa77d41facfad5a81
Harry <68451409+lujiazho@users.noreply.github.com>
2023-02-24 08:02:49 -0800

    Update 1675-minimize-deviation-in-array.cpp

--- LeetCode/1675-minimize-deviation-in-array.cpp
+++ LeetCode/1675-minimize-deviation-in-array.cpp
@@ -11,6 +11,7 @@ public:
     int minimumDeviation(vector<int>& nums) {
         priority_queue<int, vector<int>, greater<int>> heap;
         int min_ = INT_MAX;
+        // make all odd to even, and store in heap (top is the maximum because of minus sign)
         for (int i=0 ; i<nums.size() ; i++){
             if (nums[i] % 2 == 1) nums[i] *= 2;
             heap.push(-nums[i]);
@@ -18,10 +19,14 @@ public:
         }
         int res = INT_MAX;
         while (heap.top() % 2 == 0){
+            // take the maximum number
             int k = heap.top();
             heap.pop();
+            // update return val
             res = min(-k-min_, res); // each step may generate min diff
+            // update new min
             min_ = min(min_, -k/2);
+            // make it half
             heap.push(k/2);
         }
         return min(res, -heap.top()-min_); // last step could also generate min
