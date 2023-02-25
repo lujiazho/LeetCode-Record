df6d4d5c823b927a205e9946387d7b7c098df356
Harry <68451409+lujiazho@users.noreply.github.com>
2023-02-25 08:53:53 -0800

    Update 121-best-time-to-buy-and-sell-stock.cpp

--- LeetCode/121-best-time-to-buy-and-sell-stock.cpp
+++ LeetCode/121-best-time-to-buy-and-sell-stock.cpp
@@ -7,13 +7,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int dp[prices.size()][2]; // in this day's evening, i,0: hold a stock; i,1: not hold
+        int dp[prices.size()][2]; // in this day's end, i,0: hold a stock; i,1: not hold
         dp[0][0] = -prices[0];
         dp[0][1] = 0;
         for (int i=1 ; i<prices.size() ; i++){
-            dp[i][0] = max(dp[i-1][0], -prices[i]); // if buy, then discard before's get
+            dp[i][0] = max(dp[i-1][0], -prices[i]); // if buy, then discard before's stock
             dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i]);
         }
-        return max(dp[prices.size()-1][0], dp[prices.size()-1][1]);
+        // we cannot achieve maximum profit if we hold a stock
+        return dp[prices.size()-1][1];
     }
 };
