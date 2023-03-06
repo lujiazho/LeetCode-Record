d458bdb5c3d1f6f78aba0a5461dfd6c351680ee1
Harry <68451409+lujiazho@users.noreply.github.com>
2023-03-06 10:08:53 -0800

    Add files via upload

---
+++ LeetCode/1539-kth-missing-positive-number.cpp
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// interesting math and binary search
+////////////////
+// Time Complexity: O(logn)
+// Space Complexity: O(1)
+///////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+        int start = 0, end = n-1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] - (mid + 1) < k)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return start + k;
+    }
+};
