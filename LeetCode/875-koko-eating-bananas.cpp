3832e3250ebb1b492d51b8e53415507cc3e8ad95
Harry <68451409+lujiazho@users.noreply.github.com>
2023-03-08 09:49:09 -0800

    Update 875-koko-eating-bananas.cpp

--- LeetCode/875-koko-eating-bananas.cpp
+++ LeetCode/875-koko-eating-bananas.cpp
@@ -8,18 +8,13 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int l = 1, r = *max_element(piles.begin(), piles.end());
-        int min_h = 1<<31, min_eat = 1<<31;
         while (l < r){
-            int cur_eat = l+((r-l)>>1);
+            int cur_eat = (l+r)>>1;
             int cur_h = 0;
-            for (int j=0 ; j<piles.size() ; j++){
-                cur_h += ceil(piles[j]*1.0/cur_eat);
-            }
-            if (cur_h > h){
-                l = cur_eat + 1;
-            }else{
-                r = cur_eat;
-            }
+            for (int j=0 ; j<piles.size() ; j++) cur_h += ceil(piles[j]*1.0/cur_eat);
+
+            if (cur_h > h) l = cur_eat + 1;
+            else r = cur_eat;
         }
         return l;
     }
